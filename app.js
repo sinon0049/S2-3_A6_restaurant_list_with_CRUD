6bcfc6c4c464c4d63a25c96cb85d6d1508a5d3e1
wschiang <sinon049@yahoo.com.tw>
2020-11-14 00:28:08 +0800

    feat: update detail page

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
 const mongoose = require('mongoose')
+//get restaurant.js
+const Restaurant = require('./models/restaurant')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -20,22 +21,34 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
+//homepage
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: restaurantList.results })
+    Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.log(error))
 })
 
+//restaurant details
 app.get('/restaurants/:id', (req,res) => {
-    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
-    res.render('show', { restaurant: restaurant })
+    const id = req.params.id
+    return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+    
 })
 
+
+/*search function to be modified
 app.get('/search', (req, res) => {
-    const keyword = req.query.keyword
     const restaurants = restaurantList.results.filter(restaurant => {
         return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
     })
     res.render('index', {restaurants: restaurants, keyword: keyword})
 })
+*/
+
 
 app.listen(port , () => {
     console.log(`Restaurant list is running on http://localhost:${port}`)
