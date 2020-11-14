b9e427e84022799b495a9ddf7b46467ab0dddc02
wschiang <sinon049@yahoo.com.tw>
2020-11-14 09:27:19 +0800

    feat: add create and edit function

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 //get restaurant.js
 const Restaurant = require('./models/restaurant')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -39,10 +41,52 @@ app.get('/restaurants/:id', (req,res) => {
     
 })
 
+//delete restaurant
 app.post('/restaurants/:id/delete', (req, res) => {
     const id = req.params.id
     return Restaurant.findById(id)
     .then(restaurant => restaurant.remove())
+    .then(res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//create new restaurant page and create function
+app.get('/new', (req, res) => {
+    res.render('new')
+})
+
+app.post('/restaurant', (req, res) => {
+    console.log(req.body)
+    const newRestaurant = req.body
+    return Restaurant.create(newRestaurant)
+    .then(res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//edit restaurant page and edit function
+app.get('/restaurants/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', {restaurant}))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+    .then(restaurant => {
+        restaurant.name = req.body.name
+        restaurant.name_en = req.body.name_en
+        restaurant.category = req.body.category
+        restaurant.image = req.body.image
+        restaurant.location = req.body.location
+        restaurant.phone = req.body.phone
+        restaurant.google_map = req.body.google_map
+        restaurant.rating = req.body.rating
+        restaurant.description = req.body.description
+        return restaurant.save()
+    })
     .then(res.redirect('/'))
     .catch(error => console.log(error))
 })
